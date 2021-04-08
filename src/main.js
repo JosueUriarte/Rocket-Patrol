54244416208722b04fe73a29deefd2d86ea734aa
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2021-04-07 17:10:09 -0700

    added rocket movement and border collision

--- src/main.js
+++ src/main.js
@@ -12,4 +12,9 @@ let game = new Phaser.Game(config);
 // set the UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// set speed for background
 let starSpeed = 2;
+
+// reserve keyboard bindings
+let keyF, keyR, keyLEFT, keyRIGHT;
